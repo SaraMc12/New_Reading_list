b87c33ea449546f3d60305501ac47e1f98ffd0ad
Sara McMullin <mcsara1221@gmail.com>
2020-04-21 22:07:26 -0500

    Got my RSpec setup and test to pass

--- spec/Reading_list_spec.rb
+++ spec/Reading_list_spec.rb
@@ -1,24 +1,35 @@
 require 'reading_list'
-require 'net/http'
 
 
-Rspec.describe ReadingList::Book_Search do
-    context "When testing a book search" do
-        it "should return a string" do
-        user_params = ("")
-            expect().to qu()
+RSpec.describe HelloWorld do
+
+    context "When testing HelloWorld class" do
+        it "should say 'Hello World' when we call the say_hello method" do
+            hw = HelloWorld.new
+            message = hw.say_hello
+            expect(message).to eq "Hello World!"
         end
     end
 
-    context "When Book_search is called" do
-        it "should create a new table for data" do
-        end
 
-    context "When table Data is populated" do
-        it "Should set Params of bookId = index + 1" do
-            expect (top_5_results).to eq([i]+"bookTitle, bookAuthor, bookPublisher")
+
+
+   # context "When testing a book search" do
+       # it "should return a string" do
+        #user_params = ("")
+           # expect().to qu()
+       # end
+    #end
+
+   # context "When Book_search is called" do
+        #it "should create a new table for data" do
+       # end
+
+    #context "When table Data is populated" do
+        #it "Should set Params of bookId = index + 1" do
+          #  expect (top_5_results).to eq([i]+"bookTitle, bookAuthor, bookPublisher")
             
-        end
+       # end
     
 end
 
---
+++ spec/reading_list.rb
@@ -0,0 +1,10 @@
+
+
+class HelloWorld
+     def say_hello
+         "Hello World!"
+     end
+end
+
+
+
